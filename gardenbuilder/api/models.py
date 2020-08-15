8a9241125bd0f89a02519532e8317295804f98b4
David Thompson <capndavet@gmail.com>
2020-08-15 18:48:30 -0500

    added Bed class/model

--- gardenbuilder/api/models.py
+++ gardenbuilder/api/models.py
@@ -4,11 +4,27 @@ from django.contrib.auth.models import User
 # Create your models here
 class Garden(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    gardenName = models.CharField(max_length=100, blank=False)
+    garden_name = models.CharField(max_length=100, blank=False)
     owner = models.ForeignKey(User, related_name='gardens', on_delete=models.CASCADE, default=1)
 
     def __str__(self):
-        return self.gardenName
+        return self.garden_name
+
+    class Meta:
+        ordering = ['created']
+
+class Bed(models.Model):
+    bed_name = models.CharField(max_length=100, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    garden_id = models.ForeignKey(Garden, on_delete=models.CASCADE)
+    start_date = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    length = models.PositiveSmallIntegerField(default=0)
+    width = models.PositiveSmallIntegerField(default=0)
+    notes = models.CharField(max_length=500, blank=False)
+
+    def __str__(self):
+        return self.bed_name
 
     class Meta:
         ordering = ['created']
