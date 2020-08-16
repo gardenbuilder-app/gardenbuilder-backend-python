34355ec42b4a53770ec7d5bfa29b3bc7ca0b31c3
David Thompson <capndavet@gmail.com>
2020-08-16 14:15:04 -0500

    added models

--- gardenbuilder/api/models.py
+++ gardenbuilder/api/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 # Create your models here
 class Garden(models.Model):
     created = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateField(default=None)
     garden_name = models.CharField(max_length=100, blank=False)
+    start_date = models.DateField(default=now())
+    is_active = models.BooleanField(default=True)
     user_id = models.ForeignKey(User, related_name='gardens', on_delete=models.CASCADE, default=1)
 
     def __str__(self):
@@ -14,31 +18,57 @@ class Garden(models.Model):
         ordering = ['created']
 
 class Bed(models.Model):
-    bed_name = models.CharField(max_length=100, blank=False)
+    name = models.CharField(max_length=100, blank=False)
     created = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateField(default=None)
     garden_id = models.ForeignKey(Garden, related_name='beds', on_delete=models.CASCADE)
-    start_date = models.DateField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
+    start_date = models.DateField(default=now())
     length = models.PositiveSmallIntegerField(default=0)
     width = models.PositiveSmallIntegerField(default=0)
     notes = models.CharField(max_length=500, blank=False)
 
     def __str__(self):
-        return self.bed_name
+        return self.name
 
     class Meta:
         ordering = ['created']
 
 class Section(models.Model):
-    section_name = models.CharField(max_length=100, blank=False)
+    name = models.CharField(max_length=100, blank=False)
     created = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateField(default=None)
+    start_date = models.DateField(default=now())
+    is_active = models.BooleanField(default=True)
     bed_id = models.ForeignKey(Bed, related_name='sections', on_delete=models.CASCADE)
     xLocation = models.PositiveSmallIntegerField(default=0)
     yLocation = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self):
-        return self.section_name
+        return self.name
 
-    class Meta:
-        ordering = ['created']
+class Plant(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateField(default=None)
+    start_date = models.DateField(default=now())
+    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=100, blank=False)
+    section_id = models.ForeignKey(Section, related_name='plants', on_delete=models.CASCADE)
+    square_footage = models.DecimalField(default=.25, max_digits=3, decimal_places=2)
+    square_footage_sfg = models.DecimalField(default=.25, max_digits=3, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class PlantVariety(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateField(default=None)
+    start_date = models.DateField(default=now())
+    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=100, blank=False)
+    scientific_name = models.CharField(max_length=100, blank=False)
+    plant_id = models.ForeignKey(Plant, related_name='plant_varieties', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
     
