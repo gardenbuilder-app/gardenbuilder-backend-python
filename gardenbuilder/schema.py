31575c240815e829dacb249823d26fad94a8b626
David Thompson <capndavet@gmail.com>
2020-08-16 16:33:36 -0500

    changed all_gardens param to gardens

--- gardenbuilder/schema.py
+++ gardenbuilder/schema.py
@@ -5,12 +5,12 @@ from api.models import Garden
 class GardenType(DjangoObjectType):
     class Meta:
         model = Garden
-        fields = ('id', 'created', 'garden_name', 'start_date', 'end_date', 'is_active', 'user_id')
+        fields = ('id', 'created', 'garden_name', 'start_date', 'is_active', 'user_id')
 
 class Query(graphene.ObjectType):
-    all_gardens = graphene.List(GardenType)
+    gardens = graphene.List(GardenType)
     
-    def resolve_all_gardens(root, info):
+    def resolve_gardens(root, info):
         return Garden.objects.all()
 
 schema = graphene.Schema(query=Query)
