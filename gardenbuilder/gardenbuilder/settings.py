1affa5cc3404f95b040573a3a9b3e77425d559c1
Harpal <harpalassi@gmail.com>
2020-08-15 20:13:44 -0500

    remove pkg-resources from requirements

--- gardenbuilder/gardenbuilder/settings.py
+++ gardenbuilder/gardenbuilder/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
-        'PORT': '',
+        'PORT': env('DB_PORT'),
     }
 }
 
