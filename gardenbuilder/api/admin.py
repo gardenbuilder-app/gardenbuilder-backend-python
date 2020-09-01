e7ffb2bd64a2e3ad30bb15fc33f739160113b8ec
David Thompson <dthompson@tcadcentral.org>
2020-09-01 15:55:29 -0500

    restarted and completely restructured app to a more logical file/app system

--- gardenbuilder/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from api.models import Garden, Bed, Section, Plant, PlantVariety
-
-# Register your models here.
-admin.site.register(Garden)
-admin.site.register(Bed)
-admin.site.register(Section)
-admin.site.register(Plant)
-admin.site.register(PlantVariety)
