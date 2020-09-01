88016c258cda78cbd412f4941eb34b64add2c410
David Thompson <dthompson@tcadcentral.org>
2020-09-01 15:53:26 -0500

    moved gardens into its own app

---
+++ src/gardens/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from gardens.models import Garden
+
+# Register your models here.
+admin.site.register(Garden)
---
+++ src/gardens/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils.timezone import now
+from django.conf import settings
+
+class Garden(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateField(blank=True, null=True)
+    garden_name = models.CharField(max_length=100, blank=False)
+    start_date = models.DateField(default=now)
+    is_active = models.BooleanField(default=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, related_name='gardens', on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.garden_name
+
+    class Meta:
+        ordering = ['created']
