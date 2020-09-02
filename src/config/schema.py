d5ab15352f893aaa6b6798ffa28dac4021d8ca7c
David Thompson <dthompson@tcadcentral.org>
2020-09-02 12:15:44 -0500

    added sections schema to main

--- src/config/schema.py
+++ src/config/schema.py
@@ -2,12 +2,13 @@ import graphene
 import graphql_jwt
 import gardens.schema
 import beds.schema
+import sections.schema
 import users.schema
 
-class Query(beds.schema.Query, gardens.schema.Query, users.schema.Query, graphene.ObjectType):
+class Query(beds.schema.Query, gardens.schema.Query, sections.schema.Query, users.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(beds.schema.Mutation, gardens.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
+class Mutation(beds.schema.Mutation, gardens.schema.Mutation, sections.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field() 
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
