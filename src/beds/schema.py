a7ed15aeca67dc378875c10efd3e76f9852f8e08
David Thompson <dthompson@tcadcentral.org>
2020-09-02 10:55:25 -0500

    ensured optional args working, data being returned correctly

--- src/beds/schema.py
+++ src/beds/schema.py
@@ -5,51 +5,51 @@ from gardens.models import Garden
 from gardens.schema import GardenType
 from django.utils.timezone import now
 import traceback
+import datetime
 
 class BedType(DjangoObjectType):
     class Meta:
         model = Bed
 
 class CreateBed(graphene.Mutation):
+    # Define final parameter types
     id = graphene.Int()
     bed_name = graphene.String(required=True)
-    garden_id = graphene.Field(GardenType)
-    # start_date = graphene.Date(required=False)
+    garden = graphene.Field(GardenType)
+    start_date = graphene.Date(required=False)
     length = graphene.Int(required=True)
     width = graphene.Int(required=True)
     notes = graphene.String(required=False)
 
+    # Define accepted arguments
     class Arguments:
         bed_name = graphene.String(required=True)
         garden_id = graphene.Int(required=True)
-        # start_date = graphene.Date(required=False)
+        start_date = graphene.Date(required=False)
         length = graphene.Int(required=True)
         width = graphene.Int(required=True)
         notes = graphene.String(required=False)
 
     def mutate(self, info, bed_name, length, width, garden_id, **kwargs ):
-        try: 
-        
-            garden = Garden.objects.get(id=garden_id)
-            bed = Bed(bed_name=bed_name, length=length, width=width, garden=garden)
-            # start_date = kwargs.get('start_date', now)
-            notes = kwargs.get('notes', '')
-            # bed.start_date = start_date
-            bed.notes = notes
-            bed.save()
-            
-        except Exception as e:
-            print(e) 
-            print(traceback.format_exc())
-        
-        
+        # Get garden associated with id
+        garden = Garden.objects.get(id=garden_id)
+
+        # Get startDate and notes if they were optionally passed as key word arguments
+        start_date = kwargs.get('start_date', now())
+        notes = kwargs.get('notes', '')
+
+        # Save new bed with all parameters
+        bed = Bed(bed_name=bed_name, length=length, width=width, garden=garden, start_date=start_date, notes=notes)
+        bed.save()
+    
+        # Return all parameters
         return CreateBed(
-            # id=bed.id,
+            id=bed.id,
             bed_name=bed_name,
-            # garden_id=garden_id,
+            garden=garden,
             length=length,
-            # notes=notes,
-            # start_date=start_date,
+            notes=notes,
+            start_date=start_date,
             width=width
         )
 
