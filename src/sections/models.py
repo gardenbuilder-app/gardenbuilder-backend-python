ca045048b64e458110e9c55dce9fb515445f82ec
David Thompson <dthompson@tcadcentral.org>
2020-09-02 12:12:08 -0500

    wrote section models and schema

--- src/sections/models.py
+++ src/sections/models.py
@@ -4,14 +4,14 @@ from beds.models import Bed
 
 # Create your models here.
 class Section(models.Model):
-    name = models.CharField(max_length=100, blank=False)
+    # name = models.CharField(max_length=100, blank=False)
     created = models.DateTimeField(auto_now_add=True)
     end_date = models.DateField(blank=True, null=True)
     start_date = models.DateField(default=now)
     is_active = models.BooleanField(default=True)
     bed = models.ForeignKey(Bed, related_name='sections', on_delete=models.CASCADE)
-    xLocation = models.PositiveSmallIntegerField(default=0)
-    yLocation = models.PositiveSmallIntegerField(default=0)
+    xLocation = models.PositiveSmallIntegerField(default=0, blank=False)
+    yLocation = models.PositiveSmallIntegerField(default=0, blank=False)
 
     def __str__(self):
-        return self.name
+        return f'{self.xLocation}, {self.yLocation}'
