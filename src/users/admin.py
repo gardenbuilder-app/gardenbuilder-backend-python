46bf34e2ee63880014d5481a2d7b5df1b7cd1499
David Thompson <dthompson@tcadcentral.org>
2020-09-04 16:23:45 -0500

    updated CustomUserAdmin to ensure form works in admin panel

--- src/users/admin.py
+++ src/users/admin.py
@@ -8,7 +8,20 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email',]
-    ordering = ['email',]
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
