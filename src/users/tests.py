c99de801ed299fed8dd2053ab7d8933ecf9a95d9
David Thompson <capndavet@gmail.com>
2020-09-24 11:02:58 -0500

    added test_users_query_throws error to assert that querying all users cant be done by anonymous user

--- src/users/tests.py
+++ src/users/tests.py
@@ -1,11 +1,14 @@
 # from django.test import TestCase
 import pytest
+import json
+from graphene.test import Client
 from users.models import CustomUser
+from config import schema
+from graphene_django.utils.testing import GraphQLTestCase, graphql_query
 
 """
 Test whether creating a new user works
 """
-
 
 @pytest.mark.django_db
 def test_create_new_user():
@@ -19,7 +22,6 @@ def test_create_new_user():
 Test that adding username when trying to create user throws error
 """
 
-
 @pytest.mark.django_db
 def test_creating_user_with_username_throws_error():
     with pytest.raises(Exception) as e:
@@ -31,3 +33,22 @@ def test_creating_user_with_username_throws_error():
         assert (
             str(e.value) == "CustomUser() got an unexpected keyword argument 'username'"
         )
+
+class TestGraphQLQueries(GraphQLTestCase):
+    """
+    Test that querying all users isn't possible as anonymous user
+    """
+
+    def test_users_query_throws_error(self):
+        response = self.query(
+            '''
+                query {
+                    users {
+                        id
+                        email
+                    }
+                }
+            '''
+        )
+        error_message = json.loads(response.content).get('errors')[0].get('message')
+        assert 'You must be a superuser' in error_message
